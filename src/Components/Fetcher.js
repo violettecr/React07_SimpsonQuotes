ab7d8f827912632bc572cec706549e108f3496ed
violettecr <violette.devweb@gmail.com>
2019-10-29 17:23:18 +0100

    MAJ

--- src/Components/Fetcher.js
+++ src/Components/Fetcher.js
@@ -1,51 +1,52 @@
-import React from 'react';
-import axios from 'axios';
+import React from "react";
+import axios from "axios";
 
 class Fetcher extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			quote: "",
-			character: "",
-			image: "",
-			characterDirection: "",
-		};
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      quote: "",
+      character: "",
+      image: "",
+      characterDirection: ""
+    };
+  }
 
-	componentDidMount = () => {
-		axios('https://quests.wilders.dev/simpsons-quotes/quotes')
-		.then((response) =>  {
-				
-				const quotes = response.data[0].quote;
-				const characters = response.data[0].character;
-				const images = response.data[0].image;
-				const charactersDirection = response.data[0].characterDirection;
-			
+  componentDidMount = () => {
+    axios("https://quests.wilders.dev/simpsons-quotes/quotes").then(
+      response => {
+        const quotes = response.data[0].quote;
+        const characters = response.data[0].character;
+        const images = response.data[0].image;
+        const charactersDirection = response.data[0].characterDirection;
 
-			this.setState({
-				quote: quotes,
-				character: characters,
-				image: images,
-				characterDirection: charactersDirection,
-			})
-		})
-	}
+        this.setState({
+          quote: quotes,
+          character: characters,
+          image: images,
+          characterDirection: charactersDirection
+        });
+      }
+    );
+  };
 
-	render () {
-		return(
-			<div id="render">
-				<button 
-					type="button" 
-					onClick={this.componentDidMount}
-					>Refresh Page
-				</button>
-				<p>{this.state.character}</p>
-				<img src={this.state.image} alt={this.state.characters} />
-				<p>{this.state.quote}</p>
-				<p>{this.state.characterDirection}</p>
-			</div>
-		)}
+  render() {
+    return (
+      <div id="render">
+        <div id="button">
+          <button type="button" onClick={this.componentDidMount}>
+            Refresh Page
+          </button>
+        </div>
+				<div id="character">
+					<p>{this.state.character}</p>
+					<img src={this.state.image} alt={this.state.characters} />
+					<p>{this.state.quote}</p>
+					<p>{this.state.characterDirection}</p>
+				</div>
+      </div>
+    );
+  }
 }
-
 
 export default Fetcher;
